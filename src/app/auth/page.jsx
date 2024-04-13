55b1c5e5881e687c7302cb0977e493b480488ef3
Andres Felipe B <andres_242001@hotmail.com>
2024-04-13 13:25:51 -0500

    added edge to test auth page (irrelevant)

--- src/app/auth/page.jsx
+++ src/app/auth/page.jsx
@@ -1,6 +1,8 @@
 import {auth} from "@/app/api/auth/[...nextauth]/auth"
 import AuthForm from "../_components/AuthForm";
 
+export const runtime = "edge";
+
 export default async function Page() {
   const session = await auth();
     console.log(session)
